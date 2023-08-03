5bbd9897efb7d091a8417e7cd11c580dfaf21e1b
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-08-03 16:06:46 +0530

    Our Team

--- strategic/API/our_service_api.py
+++ strategic/API/our_service_api.py
@@ -1,4 +1,3 @@
-# your_app_name/api.py
 import frappe
 from frappe import _
 from frappe.utils.response import build_response
@@ -6,11 +5,20 @@ from frappe.utils.response import build_response
 @frappe.whitelist(allow_guest=True)
 def get_services():
     try:
+        # Fetch data from the parent table "Our Service"
         our_services = frappe.get_list("Our Service", fields=["name","attach", "heading", "limit", "description", "service_table"])
+
+        # Fetch data from the child table "Service Table" and add it to the result
+        for service in our_services:
+            service["service_table_data"] = frappe.get_all("Service Table", filters={"parent": service.name}, fields=["name1", "description"])
+
         return build_response("success", data=our_services)
     except Exception as e:
-        frappe.log_error(title=_("API Error"), message=e, traceback=True)
-        return build_response("error", message=_("An error occurred while fetching data."))
+        pass
+        # frappe.log_error(title="API Error", message=e, traceback=True)
+        
+
+        # return build_response("error", message=_("An error occurred while fetching data."))
 
 def build_response(status, data=None, message=None):
     response = {
