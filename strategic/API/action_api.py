18ccca46809965d14103897aacbc2cdb7bcae847
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-08-04 12:17:49 +0530

    Action

--- strategic/API/action_api.py
+++ strategic/API/action_api.py
@@ -6,7 +6,10 @@ from frappe.utils.response import build_response
 @frappe.whitelist(allow_guest=True)
 def get_actions():
     try:
-        action = frappe.get_list("Call To Action", fields=["name", "attach" ])
+        action = frappe.get_list("Call To Action", fields=["name", "attach","custom_heading","custom_description" ])
+
+        for action in action:
+            action["sector_data"] = frappe.get_all("Action Table", filters={"parent": action.name}, fields=["name1"])
         return build_response("success", data=action)
     except Exception as e:
         frappe.log_error(title=_("API Error"), message=e, traceback=True)
