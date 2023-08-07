7141213827fb99b7a2f89b31d67a0f5b811bc174
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-08-07 11:17:35 +0530

    Navbar API

--- strategic/API/navbar_api.py
+++ strategic/API/navbar_api.py
@@ -8,7 +8,7 @@ def get_navbar():
     try:
         navbar = frappe.get_list("Navbar", fields=["name1"])
         for navtab in navbar:
-            navtab["Navbar Table"] = frappe.get_all("Navbar Table", filters={"parent": navtab.name1}, fields=["name1"])
+            navtab["value"] = frappe.get_all("Navbar Table", filters={"parent": navtab.name1}, fields=["name1","url"])
         return build_response("success", data=navbar)
     except Exception as e:
         frappe.log_error(title=_("API Error"), message=e, traceback=True)
