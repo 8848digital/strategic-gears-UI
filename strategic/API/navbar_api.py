984b941e729d0276c3e96871b852874e90d3f15e
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-08-07 10:20:08 +0530

    Navbar API

--- strategic/API/navbar_api.py
+++ strategic/API/navbar_api.py
@@ -1,13 +1,15 @@
-
+# your_app_name/api.py
 import frappe
 from frappe import _
 from frappe.utils.response import build_response
 
 @frappe.whitelist(allow_guest=True)
-def get_teams():
+def get_navbar():
     try:
-        teams = frappe.get_list("Our Team", fields=["name", "attach", "designation","tittle","description,description2,description3"])
-        return build_response("success", data=teams)
+        navbar = frappe.get_list("Navbar", fields=["name1","navbar_table"])
+        for service in navbar:
+            service["Navbar Table"] = frappe.get_all("Navbar Table", filters={"parent": service.name1}, fields=["name1"])
+        return build_response("success", data=navbar)
     except Exception as e:
         frappe.log_error(title=_("API Error"), message=e, traceback=True)
         return build_response("error", message=_("An error occurred while fetching data."))
