be6a702bfd5f0a8d84323f064fb8c78d8e0970da
shyam-kumar8848 <shyamkumar@8848digital.com>
2023-08-14 15:01:08 +0530

    About Api

--- strategic/API/about_api.py
+++
@@ -1,6 +0,0 @@
-import frappe
-@frappe.whitelist()
-def get_about():
-    anout_us = frappe.get_list("About US", fields=["name", "attach", "heading","description"])
-    print(anout_us)
-    return frappe.get_list("About US", fields=["name", "attach", "heading","description"])
